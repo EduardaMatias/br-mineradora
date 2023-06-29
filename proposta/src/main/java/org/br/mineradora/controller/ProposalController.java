b600ce98df5aaf3a3b3ef0a41bca9f8ad7b2f1d4
EduardaMatias <evcm2003@gmail.com>
2023-06-29 15:15:24 -0300

    Finalizando microsserviços de proposta

---
+++ proposta/src/main/java/org/br/mineradora/controller/ProposalController.java
@@ -0,0 +1,48 @@
+package org.br.mineradora.controller;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
+import org.br.mineradora.dto.ProposalDetailsDTO;
+import org.br.mineradora.service.ProposalService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Path("/api/proposal")
+public class ProposalController {
+
+    @Inject
+    ProposalService proposalService;
+
+    private final Logger LOG = LoggerFactory.getLogger(ProposalController.class);
+
+    @GET
+    @Path("/{id}")
+    public ProposalDetailsDTO findDetailsProposal(@PathParam("id") Long id) {
+
+        return proposalService.findFullProposal(id);
+    }
+
+    @POST
+    public Response createProposal(ProposalDetailsDTO proposalDetailsDTO) {
+        LOG.info("... Recebendo nova proposta de compra");
+
+        try {
+            proposalService.createNewProposal(proposalDetailsDTO);
+            return Response.ok().build();
+        } catch (Exception e) {
+            return Response.serverError().build();
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response removeProposal(@PathParam("id") Long id) {
+        try {
+            proposalService.removeProposal(id);
+            return Response.ok().build();
+        } catch (Exception e) {
+            return Response.serverError().build();
+        }
+    }
+}
---
+++ proposta/src/main/java/org/br/mineradora/message/KafkaEvent.java
@@ -0,0 +1,21 @@
+package org.br.mineradora.message;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.br.mineradora.dto.ProposalDTO;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ApplicationScoped
+public class KafkaEvent {
+
+    private final Logger LOG = LoggerFactory.getLogger(KafkaEvent.class);
+
+    @Channel("proposal")
+    Emitter<ProposalDTO> proposalRequestEmitter;
+    public void sendNewKafkaEvent(ProposalDTO proposal) {
+        LOG.info("...Enviando nova proposta para tópico kafka");
+        proposalRequestEmitter.send(proposal).toCompletableFuture().join();
+    }
+}
